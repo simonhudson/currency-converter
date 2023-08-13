f22450a97f2c9b2277b20f5f6df54cbd5122f238
Hudson <hellosimonhudson@gmail.com>
2023-08-13 21:20:02 +0100

    fix build

--- src/helpers/debounce.js
+++ src/helpers/debounce.js
@@ -1,14 +1,15 @@
-const debounce = (func: { (): void; apply?: any }, wait?: number) => {
+const debounce = (func, wait, immediate) => {
 	wait = wait || 250;
-	let timeout: number | NodeJS.Timeout | undefined;
+	let timeout;
 	return function () {
 		const context = this;
 		const args = arguments;
 
 		const later = () => {
-			timeout = undefined;
+			timeout = null;
+			if (!immediate) func.apply(context, args);
 		};
-		const callNow = !timeout;
+		const callNow = immediate && !timeout;
 		clearTimeout(timeout);
 		timeout = setTimeout(later, wait);
 		if (callNow) func.apply(context, args);
